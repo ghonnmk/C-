0bbf0cd2e18be150644c948587fdda9faecd1ec2
ghonnmk <146837272+ghonnmk@users.noreply.github.com>
2024-06-12 20:50:59 +0700

    Add files via upload

---
+++ bai9a.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class bai9a
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập chuỗi: ");
+            string input = Console.ReadLine();
+
+            string reversedString = ReverseString(input);
+
+            Console.WriteLine("Chuỗi đảo ngược: " + reversedString);
+            Console.ReadLine();
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
